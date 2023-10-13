fa3d370c76fc220d7298419214f7030f53276855
Dylan <dylanlancekemboi@gmail.com>
2023-10-13 13:34:58 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Write your code here!
+
+// Remove the 'main' element
+const main = document.querySelector('main');
+main.remove();
+
+// Create a new 'h1' element and set its id and text content
+const newHeader = document.createElement('h1');
+newHeader.id = 'victory';
+newHeader.textContent = 'Lance is the champion';
+
+// Append the new 'h1' element to the document's body
+document.body.appendChild(newHeader);
